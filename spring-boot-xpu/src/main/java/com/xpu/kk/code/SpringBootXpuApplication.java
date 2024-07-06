1d1a4de1f7d76ae3c9978c55c31bd89a5b0f2039
kanghuarui <kanghuarui2024@163.com>
2024-07-07 00:36:13 +0800

    first commit

---
+++ spring-boot-xpu/src/main/java/com/xpu/kk/code/SpringBootXpuApplication.java
@@ -0,0 +1,13 @@
+package com.xpu.kk.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootXpuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootXpuApplication.class, args);
+    }
+
+}
---
+++ spring-boot-xpu/src/test/java/com/xpu/kk/code/SpringBootXpuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpu.kk.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootXpuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
